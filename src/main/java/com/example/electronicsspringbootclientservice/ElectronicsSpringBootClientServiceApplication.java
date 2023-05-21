36e27e2a7d7a4e4f16912e2ca1d9af9824f45a27
TAKEMI-KAZUCHI\txthai <thuyvanthai2013@gmail.com>
2023-05-21 22:26:45 +0700

    Init Project

---
+++ src/main/java/com/example/electronicsspringbootclientservice/ElectronicsSpringBootClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.electronicsspringbootclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsSpringBootClientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicsSpringBootClientServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/electronicsspringbootclientservice/ElectronicsSpringBootClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electronicsspringbootclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsSpringBootClientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
